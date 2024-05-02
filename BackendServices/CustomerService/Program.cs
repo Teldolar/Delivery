0e2162ea6dbb475d714231ee2f2db20edfff6531
Artur Gareev <arturgareev2000artur@hotmail.com>
2024-05-03 00:12:05 +0300

    Added Grpc CustomerService

--- BackendServices/CustomerService/Program.cs
+++ BackendServices/CustomerService/Program.cs
@@ -1,35 +1,14 @@
-
-using DeliveryDB;
-using DeliveryModels.DbManagers.RestaurantManagers;
-using Microsoft.EntityFrameworkCore;
+using CustomerService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DeliveryContext>(options => options.UseNpgsql(connection));
-builder.Services.AddTransient<RestaurantDbManager>();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.UseDeveloperExceptionPage();
-
-app.UseRouting();
-app.MapControllers();
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
